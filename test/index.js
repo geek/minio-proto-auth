cb14b54664d8bd2056c62b080b38707c409a2f47
geek <wpreul@gmail.com>
2017-11-21 16:25:36 -0600

    Support hapi 17

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Code = require('code');
 const Hapi = require('hapi');
 const Lab = require('lab');
 const SSO = require('../');
@@ -7,65 +8,132 @@ const SSO = require('../');
 
 const lab = exports.lab = Lab.script();
 const it = lab.it;
-const expect = lab.expect;
+const expect = Code.expect;
 
 
-it('can be registered with hapi', (done) => {
-  const server = new Hapi.Server();
-  server.connection();
-  server.register({ register: SSO, options: { sso: { privateKey: 'sso' } } }, (err) => {
-    expect(err).to.not.be.an.error();
-    server.initialize((err) => {
-      expect(err).to.not.be.an.error();
-      done();
-    });
-  });
+it('can be registered with hapi', async () => {
+  const server = Hapi.server();
+  await server.register({ plugin: SSO, options: { sso: { privateKey: 'sso' } } });
+  await server.initialize();
 });
 
-it('will protect a route', (done) => {
-  const sdcServer = new Hapi.Server();
-  sdcServer.connection({ port: 0 });
+it('will protect a route', async () => {
+  const account = {
+    id: 'b89d9dd3-62ce-4f6f-eb0d-f78e57d515d9',
+    login: 'barbar',
+    email: 'barbar@example.com',
+    companyName: 'Example Inc',
+    firstName: 'BarBar',
+    lastName: 'Jinks',
+    phone: '123-456-7890',
+    updated: '2015-12-21T11:48:54.884Z',
+    created: '2015-12-21T11:48:54.884Z'
+  };
+
+  const apiServer = Hapi.server();
+  apiServer.route({
+    method: 'GET',
+    path: '/my',
+    handler: function (request, h) {
+      return account;
+    }
+  });
+
+  await apiServer.start();
+
+  const sdcServer = Hapi.server();
   sdcServer.route({
     method: 'GET',
     path: '/session',
-    handler: (request, reply) => {
-      reply({ uuid: 'foo' });
+    handler: (request, h) => {
+      return { uuid: 'foo' };
     }
   });
-  sdcServer.start((err) => {
-    expect(err).to.not.be.an.error();
-    const server = new Hapi.Server();
-    server.connection();
+  await sdcServer.start();
 
-    const options = {
-      sso: {
-        url: `http://localhost:${sdcServer.info.port}`,
-        privateKey: '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA0UqyXDCqWDKpoNQQK/fdr0OkG4gW6DUafxdufH9GmkX/zoKz\ng/SFLrPipzSGINKWtyMvo7mPjXqqVgE10LDI3VFV8IR6fnART+AF8CW5HMBPGt/s\nfQW4W4puvBHkBxWSW1EvbecgNEIS9hTGvHXkFzm4xJ2e9DHp2xoVAjREC73B7JbF\nhc5ZGGchKw+CFmAiNysU0DmBgQcac0eg2pWoT+YGmTeQj6sRXO67n2xy/hA1DuN6\nA4WBK3wM3O4BnTG0dNbWUEbe7yAbV5gEyq57GhJIeYxRvveVDaX90LoAqM4cUH06\n6rciON0UbDHV2LP/JaH5jzBjUyCnKLLo5snlbwIDAQABAoIBAQDJm7YC3pJJUcxb\nc8x8PlHbUkJUjxzZ5MW4Zb71yLkfRYzsxrTcyQA+g+QzA4KtPY8XrZpnkgm51M8e\n+B16AcIMiBxMC6HgCF503i16LyyJiKrrDYfGy2rTK6AOJQHO3TXWJ3eT3BAGpxuS\n12K2Cq6EvQLCy79iJm7Ks+5G6EggMZPfCVdEhffRm2Epl4T7LpIAqWiUDcDfS05n\nNNfAGxxvALPn+D+kzcSF6hpmCVrFVTf9ouhvnr+0DpIIVPwSK/REAF3Ux5SQvFuL\njPmh3bGwfRtcC5d21QNrHdoBVSN2UBLmbHUpBUcOBI8FyivAWJhRfKnhTvXMFG8L\nwaXB51IZAoGBAP/E3uz6zCyN7l2j09wmbyNOi1AKvr1WSmuBJveITouwblnRSdvc\nsYm4YYE0Vb94AG4n7JIfZLKtTN0xvnCo8tYjrdwMJyGfEfMGCQQ9MpOBXAkVVZvP\ne2k4zHNNsfvSc38UNSt7K0HkVuH5BkRBQeskcsyMeu0qK4wQwdtiCoBDAoGBANF7\nFMppYxSW4ir7Jvkh0P8bP/Z7AtaSmkX7iMmUYT+gMFB5EKqFTQjNQgSJxS/uHVDE\nSC5co8WGHnRk7YH2Pp+Ty1fHfXNWyoOOzNEWvg6CFeMHW2o+/qZd4Z5Fep6qCLaa\nFvzWWC2S5YslEaaP8DQ74aAX4o+/TECrxi0z2lllAoGAdRB6qCSyRsI/k4Rkd6Lv\nw00z3lLMsoRIU6QtXaZ5rN335Awyrfr5F3vYxPZbOOOH7uM/GDJeOJmxUJxv+cia\nPQDflpPJZU4VPRJKFjKcb38JzO6C3Gm+po5kpXGuQQA19LgfDeO2DNaiHZOJFrx3\nm1R3Zr/1k491lwokcHETNVkCgYBPLjrZl6Q/8BhlLrG4kbOx+dbfj/euq5NsyHsX\n1uI7bo1Una5TBjfsD8nYdUr3pwWltcui2pl83Ak+7bdo3G8nWnIOJ/WfVzsNJzj7\n/6CvUzR6sBk5u739nJbfgFutBZBtlSkDQPHrqA7j3Ysibl3ZIJlULjMRKrnj6Ans\npCDwkQKBgQCM7gu3p7veYwCZaxqDMz5/GGFUB1My7sK0hcT7/oH61yw3O8pOekee\nuctI1R3NOudn1cs5TAy/aypgLDYTUGQTiBRILeMiZnOrvQQB9cEf7TFgDoRNCcDs\nV/ZWiegVB/WY7H0BkCekuq5bHwjgtJTpvHGqQ9YD7RhE8RSYOhdQ/Q==\n-----END RSA PRIVATE KEY-----\n'
+  const server = Hapi.server();
+  const options = {
+    sso: {
+      url: `http://localhost:${sdcServer.info.port}`,
+      apiBaseUrl: `http://localhost:${apiServer.info.port}`,
+      permissions: { portal: true },
+      privateKey: '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA0UqyXDCqWDKpoNQQK/fdr0OkG4gW6DUafxdufH9GmkX/zoKz\ng/SFLrPipzSGINKWtyMvo7mPjXqqVgE10LDI3VFV8IR6fnART+AF8CW5HMBPGt/s\nfQW4W4puvBHkBxWSW1EvbecgNEIS9hTGvHXkFzm4xJ2e9DHp2xoVAjREC73B7JbF\nhc5ZGGchKw+CFmAiNysU0DmBgQcac0eg2pWoT+YGmTeQj6sRXO67n2xy/hA1DuN6\nA4WBK3wM3O4BnTG0dNbWUEbe7yAbV5gEyq57GhJIeYxRvveVDaX90LoAqM4cUH06\n6rciON0UbDHV2LP/JaH5jzBjUyCnKLLo5snlbwIDAQABAoIBAQDJm7YC3pJJUcxb\nc8x8PlHbUkJUjxzZ5MW4Zb71yLkfRYzsxrTcyQA+g+QzA4KtPY8XrZpnkgm51M8e\n+B16AcIMiBxMC6HgCF503i16LyyJiKrrDYfGy2rTK6AOJQHO3TXWJ3eT3BAGpxuS\n12K2Cq6EvQLCy79iJm7Ks+5G6EggMZPfCVdEhffRm2Epl4T7LpIAqWiUDcDfS05n\nNNfAGxxvALPn+D+kzcSF6hpmCVrFVTf9ouhvnr+0DpIIVPwSK/REAF3Ux5SQvFuL\njPmh3bGwfRtcC5d21QNrHdoBVSN2UBLmbHUpBUcOBI8FyivAWJhRfKnhTvXMFG8L\nwaXB51IZAoGBAP/E3uz6zCyN7l2j09wmbyNOi1AKvr1WSmuBJveITouwblnRSdvc\nsYm4YYE0Vb94AG4n7JIfZLKtTN0xvnCo8tYjrdwMJyGfEfMGCQQ9MpOBXAkVVZvP\ne2k4zHNNsfvSc38UNSt7K0HkVuH5BkRBQeskcsyMeu0qK4wQwdtiCoBDAoGBANF7\nFMppYxSW4ir7Jvkh0P8bP/Z7AtaSmkX7iMmUYT+gMFB5EKqFTQjNQgSJxS/uHVDE\nSC5co8WGHnRk7YH2Pp+Ty1fHfXNWyoOOzNEWvg6CFeMHW2o+/qZd4Z5Fep6qCLaa\nFvzWWC2S5YslEaaP8DQ74aAX4o+/TECrxi0z2lllAoGAdRB6qCSyRsI/k4Rkd6Lv\nw00z3lLMsoRIU6QtXaZ5rN335Awyrfr5F3vYxPZbOOOH7uM/GDJeOJmxUJxv+cia\nPQDflpPJZU4VPRJKFjKcb38JzO6C3Gm+po5kpXGuQQA19LgfDeO2DNaiHZOJFrx3\nm1R3Zr/1k491lwokcHETNVkCgYBPLjrZl6Q/8BhlLrG4kbOx+dbfj/euq5NsyHsX\n1uI7bo1Una5TBjfsD8nYdUr3pwWltcui2pl83Ak+7bdo3G8nWnIOJ/WfVzsNJzj7\n/6CvUzR6sBk5u739nJbfgFutBZBtlSkDQPHrqA7j3Ysibl3ZIJlULjMRKrnj6Ans\npCDwkQKBgQCM7gu3p7veYwCZaxqDMz5/GGFUB1My7sK0hcT7/oH61yw3O8pOekee\nuctI1R3NOudn1cs5TAy/aypgLDYTUGQTiBRILeMiZnOrvQQB9cEf7TFgDoRNCcDs\nV/ZWiegVB/WY7H0BkCekuq5bHwjgtJTpvHGqQ9YD7RhE8RSYOhdQ/Q==\n-----END RSA PRIVATE KEY-----\n'
+    }
+  };
+
+  await server.register({ plugin: SSO, options });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: 'sso',
+      handler: function (request, h) {
+        return request.auth.credentials.profile.id;
       }
-    };
-
-    server.register({ register: SSO, options }, (err) => {
-      expect(err).to.not.be.an.error();
-
-      server.route({
-        method: 'GET',
-        path: '/',
-        config: {
-          auth: 'sso',
-          handler: function (request, reply) {
-            reply('hello, ' + request.auth.user.uuid);
-          }
-        }
-      });
-
-      server.initialize((err) => {
-        expect(err).to.not.be.an.error();
-        server.inject('/', (res) => {
-          expect(res.statusCode).to.equal(302);
-          expect(res.headers.location).to.contain('sig=');
-          sdcServer.stop(done);
-        });
-      });
-    });
+    }
   });
+
+  await server.initialize();
+  const res = await server.inject('/');
+  expect(res.statusCode).to.equal(302);
+  expect(res.headers.location).to.contain('sig=');
+
+  const authRes = await server.inject('/?token=something');
+  expect(authRes.payload).to.equal(account.id);
+  await sdcServer.stop();
+  await apiServer.stop();
+});
+
+it('will login the local user if in dev mode', async () => {
+  const account = {
+    id: 'b89d9dd3-62ce-4f6f-eb0d-f78e57d515d9',
+    login: 'barbar',
+    email: 'barbar@example.com',
+    companyName: 'Example Inc',
+    firstName: 'BarBar',
+    lastName: 'Jinks',
+    phone: '123-456-7890',
+    updated: '2015-12-21T11:48:54.884Z',
+    created: '2015-12-21T11:48:54.884Z'
+  };
+
+  const apiServer = Hapi.server();
+  apiServer.route({
+    method: 'GET',
+    path: '/my',
+    handler: function (request, h) {
+      return account;
+    }
+  });
+
+  await apiServer.start();
+  const server = Hapi.server();
+
+  const options = {
+    sso: {
+      apiBaseUrl: `http://localhost:${apiServer.info.port}`,
+      isDev: true,
+      privateKey: '-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEA0UqyXDCqWDKpoNQQK/fdr0OkG4gW6DUafxdufH9GmkX/zoKz\ng/SFLrPipzSGINKWtyMvo7mPjXqqVgE10LDI3VFV8IR6fnART+AF8CW5HMBPGt/s\nfQW4W4puvBHkBxWSW1EvbecgNEIS9hTGvHXkFzm4xJ2e9DHp2xoVAjREC73B7JbF\nhc5ZGGchKw+CFmAiNysU0DmBgQcac0eg2pWoT+YGmTeQj6sRXO67n2xy/hA1DuN6\nA4WBK3wM3O4BnTG0dNbWUEbe7yAbV5gEyq57GhJIeYxRvveVDaX90LoAqM4cUH06\n6rciON0UbDHV2LP/JaH5jzBjUyCnKLLo5snlbwIDAQABAoIBAQDJm7YC3pJJUcxb\nc8x8PlHbUkJUjxzZ5MW4Zb71yLkfRYzsxrTcyQA+g+QzA4KtPY8XrZpnkgm51M8e\n+B16AcIMiBxMC6HgCF503i16LyyJiKrrDYfGy2rTK6AOJQHO3TXWJ3eT3BAGpxuS\n12K2Cq6EvQLCy79iJm7Ks+5G6EggMZPfCVdEhffRm2Epl4T7LpIAqWiUDcDfS05n\nNNfAGxxvALPn+D+kzcSF6hpmCVrFVTf9ouhvnr+0DpIIVPwSK/REAF3Ux5SQvFuL\njPmh3bGwfRtcC5d21QNrHdoBVSN2UBLmbHUpBUcOBI8FyivAWJhRfKnhTvXMFG8L\nwaXB51IZAoGBAP/E3uz6zCyN7l2j09wmbyNOi1AKvr1WSmuBJveITouwblnRSdvc\nsYm4YYE0Vb94AG4n7JIfZLKtTN0xvnCo8tYjrdwMJyGfEfMGCQQ9MpOBXAkVVZvP\ne2k4zHNNsfvSc38UNSt7K0HkVuH5BkRBQeskcsyMeu0qK4wQwdtiCoBDAoGBANF7\nFMppYxSW4ir7Jvkh0P8bP/Z7AtaSmkX7iMmUYT+gMFB5EKqFTQjNQgSJxS/uHVDE\nSC5co8WGHnRk7YH2Pp+Ty1fHfXNWyoOOzNEWvg6CFeMHW2o+/qZd4Z5Fep6qCLaa\nFvzWWC2S5YslEaaP8DQ74aAX4o+/TECrxi0z2lllAoGAdRB6qCSyRsI/k4Rkd6Lv\nw00z3lLMsoRIU6QtXaZ5rN335Awyrfr5F3vYxPZbOOOH7uM/GDJeOJmxUJxv+cia\nPQDflpPJZU4VPRJKFjKcb38JzO6C3Gm+po5kpXGuQQA19LgfDeO2DNaiHZOJFrx3\nm1R3Zr/1k491lwokcHETNVkCgYBPLjrZl6Q/8BhlLrG4kbOx+dbfj/euq5NsyHsX\n1uI7bo1Una5TBjfsD8nYdUr3pwWltcui2pl83Ak+7bdo3G8nWnIOJ/WfVzsNJzj7\n/6CvUzR6sBk5u739nJbfgFutBZBtlSkDQPHrqA7j3Ysibl3ZIJlULjMRKrnj6Ans\npCDwkQKBgQCM7gu3p7veYwCZaxqDMz5/GGFUB1My7sK0hcT7/oH61yw3O8pOekee\nuctI1R3NOudn1cs5TAy/aypgLDYTUGQTiBRILeMiZnOrvQQB9cEf7TFgDoRNCcDs\nV/ZWiegVB/WY7H0BkCekuq5bHwjgtJTpvHGqQ9YD7RhE8RSYOhdQ/Q==\n-----END RSA PRIVATE KEY-----\n'
+    }
+  };
+
+  await server.register({ plugin: SSO, options });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: 'sso',
+      handler: function (request, h) {
+        return request.auth.credentials.profile;
+      }
+    }
+  });
+
+  await server.initialize();
+  const res = await server.inject('/');
+  expect(res.statusCode).to.equal(200);
+  expect(res.payload).to.contain(account.id);
+  await apiServer.stop();
 });
